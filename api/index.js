d4883827e4dfc0e0840923542808d36c8566f0ac
WalidB <walid.boulboul59@gmail.com>
2024-02-25 23:57:51 +0100

    add quizz socket logic

--- api/index.js
+++ api/index.js
@@ -9,8 +9,12 @@ const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 const quizRouter = require('./routes/quiz');
 const sessionRouter = require('./routes/session');
-const { verifyToken } = require('./lib/token');
+const subscriptionRouter = require('./routes/subscription');
 const isAuthenticatedSocket = require('./middlewares/isAuthenticatedSocket');
+const Session = require('./models/session');
+const { SESSION_STATUSES, SUBSCRIPTION_STATUSES } = require('./lib/constants');
+const Quiz = require('./models/quiz');
+const Subscription = require('./models/subscription');
 
 /**
  * Initialize express app & servers
@@ -63,14 +67,143 @@ app.get('/', function (_req, res) {
 app.use('/', authRouter);
 app.use('/quizzes', quizRouter);
 app.use('/sessions', sessionRouter);
+app.use('/subscriptions', subscriptionRouter);
 
 /**
  * Run socket server
  */
 
+function formatQuizToSubscription(quiz) {
+	return quiz.toJSON().questions.map(({ points, ...question }) => ({
+		...question,
+	}));
+}
+
 io.on('connection', async function (socket) {
 	console.log(`🚀 ${socket.id} user connected successfully`);
-	console.log('User', socket.user);
+
+	socket.on('SESSION_JOIN', async function (code) {
+		const session = await Session.findOne({
+			code,
+			status: SESSION_STATUSES.ACTIVE,
+		});
+
+		if (!session) {
+			socket.emit('SESSION_JOIN_ERROR', 'Invalid session code');
+		}
+
+		const quiz = await Quiz.findOne({
+			_id: session.quizId,
+		});
+
+		const subscription = await Subscription.findOne({
+			userId: socket.user.id,
+			status: SUBSCRIPTION_STATUSES.ACTIVE,
+		});
+
+		if (subscription) {
+			socket.emit('SESSION_JOIN_ERROR', 'You have already a running session');
+		}
+
+		await Subscription.create({
+			userId: socket.user.id,
+			sessionId: session._id,
+			questions: formatQuizToSubscription(quiz),
+		});
+
+		socket.emit('SESSION_JOIN_SUCCESS', null);
+	});
+
+	socket.on('QUESTION_ANSWER', async function (data) {
+		const subscription = await Subscription.findOne({
+			_id: data.subscriptionId,
+			userId: socket.user.id,
+			status: SUBSCRIPTION_STATUSES.ACTIVE,
+		});
+		if (!subscription) {
+			return socket.emit('QUESTION_ANSWER_ERROR', 'Subscription not found');
+		}
+
+		const question = subscription.questions.find(
+			(q) => q._id.toHexString() === data.questionId,
+		);
+		console.log('question', question);
+		if (question.isAnswered) {
+			return socket.emit(
+				'QUESTION_ANSWER_ERROR',
+				'You already answered this question',
+			);
+		}
+
+		const session = await Session.findOne({
+			_id: subscription.sessionId,
+			status: SESSION_STATUSES.ACTIVE,
+		});
+		if (!session) {
+			return socket.emit('QUESTION_ANSWER_ERROR', 'Session not found');
+		}
+
+		const quiz = await Quiz.findOne({
+			_id: session.quizId,
+			status: SESSION_STATUSES.ACTIVE,
+		}).select('+questions.answers.isCorrect');
+		if (!quiz) {
+			return socket.emit('QUESTION_ANSWER_ERROR', 'Quiz not found');
+		}
+
+		const quizQuestion = quiz.questions.find((q) => (q._id = data.questionId));
+
+		let isCorrect = true;
+		for (const answer of data.answers) {
+			const a = quizQuestion.answers.find(
+				(an) => an._id.toHexString() === answer._id,
+			);
+			if (a && a.isCorrect !== answer.isSelected) {
+				isCorrect = false;
+			}
+		}
+
+		const newSubscription = await Subscription.findOneAndUpdate(
+			{
+				_id: subscription.id,
+				'questions._id': data.questionId,
+			},
+			{
+				$set: {
+					'questions.$.isAnswered': true,
+					'questions.$.isCorrect': isCorrect,
+					'questions.$.answers': data.answers,
+				},
+			},
+			{ new: true },
+		);
+
+		const numOfQuestions = newSubscription.questions.filter(
+			(q) => q.isAnswered === false,
+		).length;
+
+		if (numOfQuestions > 0) {
+			const nextQuestion = newSubscription.questions.find((q) => !q.isAnswered);
+			const payload = {
+				_id: newSubscription._id,
+				question: nextQuestion,
+				hasMoreQuestions: numOfQuestions >= 2,
+			};
+			isCorrect
+				? socket.emit('QUESTION_NEXT_SUCCESS', payload)
+				: socket.emit('QUESTION_NEXT_ERROR', payload);
+		} else {
+			await Subscription.findOneAndUpdate(
+				{ _id: subscription.id },
+				{ status: 'DONE' },
+				{ new: true },
+			);
+
+			isCorrect
+				? socket.emit('QUESTION_DONE_SUCCESS', newSubscription._id)
+				: socket.emit('QUESTION_DONE_ERROR', newSubscription._id);
+		}
+	});
 
 	socket.on('disconnect', function () {
 		console.log(`💤 ${socket.id} user disconnected successfully`);
